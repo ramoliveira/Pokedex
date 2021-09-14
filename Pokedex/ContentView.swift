e46e68481799f07b5b1967842e3bfd0addd9f1ea
Ramón <dias.ramon07@hotmail.com>
2021-09-13 21:34:43 -0300

    initial commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Ramon Dias on 13/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Pokedex coming")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
